8a77b52acfaf60dc54da6b62928d49c4c2b2ce5b
designpigofficial <89279319+designpigofficial@users.noreply.github.com>
2022-07-24 01:19:13 -0600

    init

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,5 +1,9 @@
+import Text from '@chakra-ui/react'
+
 function Footer() {
-  return <></>;
+  return <>
+  <Text size="small" style="capitalize">&copy; 2022 Abner Development </Text>
+  </>;
 }
 
 export default Footer;
